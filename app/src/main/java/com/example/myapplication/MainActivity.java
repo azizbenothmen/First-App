ae5b41909c6df535aa9ccf70baf26774d6fb20b7
azizbenothmen <aziz.benothmen@enicar.ucar.tn>
2026-02-17 23:24:38 +0100

    First Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount=1;
+    private TextView mshowCount;
+    private String msg="AZIZ";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        Log.d("Tag",msg);
+
+        setContentView(R.layout.activity_main);
+        mshowCount=(TextView) findViewById(R.id.showcount);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void showToast(View view) {
+        Toast toast= Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void showCount(View view) {
+        ++mCount;
+        Log.d("Tag",msg);
+        if(mshowCount!=null){
+            mshowCount.setText(Integer.toString(mCount));
+            Log.d("Tag",msg);
+        }
+    }
+}
